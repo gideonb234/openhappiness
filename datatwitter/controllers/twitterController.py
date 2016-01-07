cabf152336c3ec535103b4781a86b7a39874bf4b
Gideon <gideonb234@gmail.com>
2016-01-07 14:51:44 +0000

    twitter api test success

--- datatwitter/controllers/twitterController.py
+++ datatwitter/controllers/twitterController.py
@@ -1,1 +1,10 @@
 from TwitterAPI import TwitterAPI
+
+#Twitter API stuff
+
+
+api = TwitterAPI(consumer_key, consumer_secret, access_token, access_secret)
+
+r = api.request('search/tweets', {'q':'pizza'})
+for item in r:
+    print(item)
