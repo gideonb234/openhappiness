87bdd1d09c48bfc91a305462c8eb65675af4dd28
Gideon <gideonb234@gmail.com>
2016-01-07 13:26:03 +0000

    models added

--- datatwitter/models.py
+++ datatwitter/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+# class Dataset (models.Model):
+
+class Tweet (models.Model):
+    tweet_id = "id" #foreign key
+    tweet_text = "tweet"
+    tweet_datetime = "datetime"
+    tweet_location = "location" #can be nulled
+
+class Twitter (models.Model):
+    tweet = Tweet.tweet_id
+    datetime = datetime.now()
