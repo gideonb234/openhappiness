1bbe27b672f41355df88579a4df2ff2f200aec55
Gideon <gideonb234@gmail.com>
2016-01-11 13:09:39 +0000

    add twitter keys, start OOPing the controllers

--- datatwitter/controllers/fileController.py
+++ datatwitter/controllers/fileController.py
@@ -1,2 +1,12 @@
 #import io here and a josn/csv parsing package?
 import json, io, csv
+
+class Dataset:
+    def __init__(self, filename, data_type):
+        #filename must relate to file path
+        self.filename = filename
+        self.data_type = data_type
+        Dataset.analysed = False
+
+    def isAnalysed(self):
+        return Dataset.isAnalysed()
--- datatwitter/controllers/twitterController.py
+++ datatwitter/controllers/twitterController.py
@@ -1,10 +1,27 @@
 from TwitterAPI import TwitterAPI
 
+from . import twitterKeys
+
 #Twitter API stuff
+consumer_key = twitterKeys.consumer_key
+consumer_secret = twitterKeys.consumer_secret
+access_token = twitterKeys.access_token
+access_secret = twitterKeys.access_secret
+
+class TwitterController:
+    def __init__(self):
+        self.api = TwitterAPI(consumer_key, consumer_secret, access_token, access_secret)
+
+    def search_tweets(self, query):
+        search_query = self.api.request('search/tweets', {'q': query})
+        for item in search_query:
+            print(item)
+
+    def search_user(self,user):
+        search_query = self.api.request('search/users', {'q' : user})
+        for item in search_query:
+            print(item)
 
 
-api = TwitterAPI(consumer_key, consumer_secret, access_token, access_secret)
-
-r = api.request('search/tweets', {'q':'pizza'})
-for item in r:
-    print(item)
+test = TwitterController
+test.search_tweets("happiness")
