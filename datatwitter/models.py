c5370c174c979bd9d40d1d621cf883b2c65634d1
Gideon <gideonb234@gmail.com>
2016-02-15 14:28:51 +0000

    model change + final poc view addition

--- datatwitter/models.py
+++ datatwitter/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from datetime import datetime
+from django.core.files.storage import FileSystemStorage
 
 # Create your models here.
 
 # class Dataset (models.Model):
+
 
 class Tweet (models.Model):
     tweet_id = "id" #foreign key
@@ -11,10 +13,14 @@ class Tweet (models.Model):
     tweet_datetime = "datetime"
     tweet_location = "location" #can be nulled
 
+
 class Twitter (models.Model):
-    tweet = models.ForeignKey(Tweet)
+    tweet = models.ForeignKey(Tweet, default="00")
     datetime = datetime.now()
 
+fs = FileSystemStorage(location='/datatwitter/files/')
+
+
 class Files(models.Model):
-    id
-    file_path = models.FileField(upload_to="%y%m%d/%f")
+    file_title = models.TextField(max_length=100, default="Untitled")
+    file_path = models.FileField(upload_to="%y%m%d/%f", storage=fs)
--- datatwitter/views.py
+++ datatwitter/views.py
@@ -34,7 +34,7 @@ def poc(request):
         elif request.POST['form-type'] == 'dataset-form':
             form = UploadFileForm(request.POST, request.FILES)
             if form.is_valid():
-                instance = Files(file_path=request.FILES['file'])
+                instance = Files(file_path=request.FILES['file'], file_title=request.POST['title'])
                 instance.save()
                 print("file is written")
                 return HttpResponseRedirect('/datatwitter/poc/')
