dff3ea362c3c93d6e174060e3d70d016b75994b1
Gideon <gideonb234@gmail.com>
2016-02-16 17:33:01 +0000

    what is static urls

--- datatwitter/urls.py
+++ datatwitter/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 app_name = "datatwitter"
+
 urlpatterns = [
     url(r'poc/', views.poc, name="poc"),
     url(r'index', views.index, name="index"),
--- open_happy/settings.py
+++ open_happy/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = 'static/'
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-STATICFILES_DIRS = (
-    os.path.join(SITE_ROOT, '/templates/static/')
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "templates/static/")
+]
 
 # Uploaded Files (Datasets etc)
 
