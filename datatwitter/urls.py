e569e258a21c454b389efaf4fb849932174d063f
Gideon <gideonb234@gmail.com>
2016-03-03 18:31:23 +0000

    url linking

--- datatwitter/urls.py
+++ datatwitter/urls.py
@@ -9,6 +9,10 @@ app_name = "datatwitter"
 urlpatterns = [
     url(r'poc', views.poc, name="poc"),
     url(r'index', views.index, name="index"),
+    url(r'start', views.dataset_upload, name="dataset"),
+    url(r'twitter', views.twitter_query, name="twitter"),
+    url(r'comparison', views.comparison, name="comparison"),
+    url(r'output', views.output_view, name="output"),
     url(r'^dataset/(?P<dataset_id>[0-9]+)', views.dataset, name="files"),
     url(r'', views.index, name="default"),
 ]
