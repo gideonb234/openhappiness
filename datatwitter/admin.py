dc263bb0b08113395cacf4952d91be7d1b2d9f94
Gideon <gideonb234@gmail.com>
2016-03-18 15:17:48 +0000

    admin page changed

--- datatwitter/admin.py
+++ datatwitter/admin.py
@@ -7,4 +7,5 @@ from .models import *
 admin.site.register(Tweet)
 admin.site.register(Twitter)
 admin.site.register(Dataset)
-admin.site.register(Result)
+admin.site.register(DatasetResult)
+admin.site.register(QueryResult)
