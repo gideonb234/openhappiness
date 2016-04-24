b7ce6dd90a8dd36479c77cc7f4394c684c332158
Gideon <gideonb234@gmail.com>
2016-04-24 16:35:58 +0100

    more documentation

--- datatwitter/models.py
+++ datatwitter/models.py
@@ -8,13 +8,15 @@ import json
 
 # class Dataset (models.Model):
 
-
+# Save tweets to here
 class Tweets (models.Model):
     tweet_text = ArrayField(models.IntegerField(),size=100, default=[1,2,3,4], blank=True)
-
+# define a place for file storage
 fs = FileSystemStorage(location='/datatwitter/files/')
 
+# keeps a track of datasets saved to the server
 class Dataset(models.Model):
+    # define a file path for the data to be stored in
     def content_name(self, filename):
         return '/'.join(['datasets', self.file_title, filename])
 
@@ -22,20 +24,24 @@ class Dataset(models.Model):
     fs = FileSystemStorage()
     file_path = models.FileField(upload_to=content_name, storage=fs)
 
+    # this is what it Dataset returns when it is called by other functions
     def __str__(self):
         return self.file_title
 
+    # upload dataset to the server
     def upload(self, file_title, file_path):
         instance = Dataset(file_path=file_path, file_title=file_title)
         instance.save()
         return instance.pk
 
+    # remove dataset from the server
     def remove(self, file_id):
         if file_id == self.objects.get(file_id):
             file = self.objects.get(file_id)
             file.delete()
 
-
+# store results from comparison (dataset version)
+# Major difference is the link to the Dataset model
 class DatasetResult(models.Model):
     datetime = datetime.now()
     average = models.BigIntegerField()
@@ -45,13 +51,15 @@ class DatasetResult(models.Model):
     dataset = models.ForeignKey(Dataset)
     dataset_sentiment = ArrayField(models.IntegerField(), size=100, default=[1,2,3,4], blank=True)
 
+    # save results to the database server and return the primary key for the instance
     def upload(self, dataset_result):
-        # saves dataset result to the database
         instance = DatasetResult(average=dataset_result[1], d_range=dataset_result[2], median=dataset_result[3],
                                  sentiment=dataset_result[0], dataset_id=dataset_result[4])
         instance.save()
         return instance.pk
 
+# store results from comparison (twitter query version)
+# Major difference is able to save the query and the tweets from it
 class QueryResult(models.Model):
     datetime = datetime.now()
     average = models.BigIntegerField()
@@ -61,7 +69,7 @@ class QueryResult(models.Model):
     sentiment = models.CharField(max_length=20)
     twitter_query = models.CharField(max_length=100)
     twitter_fk = models.ForeignKey(Tweets, null=True)
-
+    # save results to the database server and return the primary key for the instance
     def upload(self, query_result):
         instance = QueryResult(average=query_result[1], d_range=query_result[2], median=query_result[3],
                                sentiment=query_result[0], twitter_query=query_result[4])
